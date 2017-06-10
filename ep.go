4d378bbdda33fbf9a0ecad794ea22b98ab793eac
Roi Avinoam <avinoamr@gmail.com>
2017-06-10 16:24:56 +0300

    Package docs

--- ep.go
+++ ep.go
@@ -1,5 +1,4 @@
-// Distributed Query Processing &amp; Execution Framework
-//
-// ep is a framework designed to make it easy to construct complex query engines
-// and data processing pipelines that are distributed across a cluster of nodes.
+// Distributed Query Processing & Execution Framework that makes it easy to
+// construct complex query engines and data processing pipelines that are
+// distributed across a cluster of nodes.
 package ep
