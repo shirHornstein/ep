e7cc0ad0dd0d140ce4db0708be8afa1345a704dc
Roi Avinoam <avinoamr@gmail.com>
2017-06-11 10:59:41 +0300

    Node API

--- node.go
+++ node.go
@@ -10,6 +10,14 @@ import (
 // provides the API required for communicating with the node.
 type Node interface {
 
+    // Connect to the node, denoted by a special UUID. It MUST be the case that
+    // the same UUID would always return the same connection, on both sides of
+    // the connection. The UUID is the mechanism used to bind the two nodes in
+    // a specific logic location of the code, and thus must be consistent.
+    // When the connection is closed, it's safe to assume that no additional
+    // Connect() calls will arrive for the same UUID, thus its mapping can be
+    // safely removed from memory.
+    Connect(uuid string) (net.Conn, error)
 }
 
 
