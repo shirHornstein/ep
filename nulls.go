6cd5357bf450c6c55dbc40fb23ab9a9f487565ac
Roi Avinoam <avinoamr@gmail.com>
2017-06-12 20:02:27 +0300

    Nulls implementation

--- nulls.go
+++ nulls.go
@@ -2,7 +2,18 @@ package ep
 
 // Nulls is a Type representing NULL values. Use Nulls.Data(n) to create Data
 // instances of `n` nulls
-var Nulls = nulls(0)
+var Null = &nullType{}
+
+type nullType struct {}
+func (*nullType) Data(n int) Data { return nulls(n) }
+func (*nullType) Name() string { return "NULL" }
 
 // nulls is implemented to satisfy both the Type and Data interfaces
 type nulls int // number of nulls in the set
+func (nulls) Type() Type { return Null }
+func (nulls) Less(int, int) bool { return false }
+func (nulls) Swap(int, int) {}
+func (nulls) Slice(i, j int) Data { return nulls(j - i) }
+func (vs nulls) Append(data Data) Data { return vs + data.(nulls) }
+func (vs nulls) Len() int { return int(vs) }
+func (vs nulls) Strings() []string { return make([]string, vs) }
