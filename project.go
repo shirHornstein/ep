cfcc15e4e2ad99d55972386b32899b5707a0a591
Roi Avinoam <avinoamr@gmail.com>
2017-06-16 17:50:50 +0300

    Fix compile errors

--- project.go
+++ project.go
@@ -1,7 +1,11 @@
 package ep
 
+import (
+    "context"
+)
+
 // Project returns a horizontal composite projection runner that dispatches
-// its input to all of the internal streams, and joins the result into a single
+// its input to all of the internal runners, and joins the result into a single
 // dataset to return.
 func Project(runners ...Runner) Runner {
     return project(runners)
@@ -9,10 +13,10 @@ func Project(runners ...Runner) Runner {
 
 type project []Runner
 
-// Run dispatches the same input to all inner streams, and then collects and
+// Run dispatches the same input to all inner runners, and then collects and
 // joins their results into a single dataset output
-func (streams project) Run(ctx Ctx, inp, out chan Dataset) (err error) {
-    if len(streams) == 0 {
+func (rs project) Run(ctx context.Context, inp, out chan Dataset) (err error) {
+    if len(rs) == 0 {
         // NB Do we really want this? Shouldn't we just fail for an empty
         // projection? At the time of writing it only happened due to bugs in
         // the code - not due to any realistic use-case
@@ -20,23 +24,23 @@ func (streams project) Run(ctx Ctx, inp, out chan Dataset) (err error) {
         return nil
     }
 
-    // Run all inner streams
-    inputs := make([]chan Dataset, len(streams))
-    outputs := make([]chan Dataset, len(streams))
-    for i := range streams {
+    // Run all inner runners
+    inputs := make([]chan Dataset, len(rs))
+    outputs := make([]chan Dataset, len(rs))
+    for i := range rs {
         inputs[i] = make(chan Dataset)
         outputs[i] = make(chan Dataset)
 
         go func(i int) {
             defer close(outputs[i])
-            err1 := streams[i].Run(ctx, inputs[i], outputs[i])
+            err1 := rs[i].Run(ctx, inputs[i], outputs[i])
             if err1 != nil && err == nil {
                 err = err1
             }
         }(i)
     }
 
-    // Dispatch input to all inner streams
+    // Dispatch input to all inner runners
     go func() {
         for data := range inp {
             for _, s := range inputs {
@@ -44,13 +48,13 @@ func (streams project) Run(ctx Ctx, inp, out chan Dataset) (err error) {
             }
         }
 
-        // close all inner streams
+        // close all inner runners
         for _, s := range inputs {
             close(s)
         }
     }()
 
-    // collect the output from all inner streams, in order.
+    // collect the output from all inner runners, in order.
     //
     // TODO this assumes uniformity of all composed functions such that the
     // datasets are of equal lengths. Of course this assumption is wrong, and
@@ -58,11 +62,13 @@ func (streams project) Run(ctx Ctx, inp, out chan Dataset) (err error) {
     // functions
     done := false
     for !done {
-        result := Dataset{}
+        result := dataset{}
         for _, s := range outputs {
             res, ok := <- s
             done = done || !ok
-            result = append(result, res...)
+            for i := 0; i < res.Width(); i++ {
+                result = append(result, res.At(i))
+            }
         }
 
         if result.Width() > 0 && result.Len() > 0 {
@@ -71,4 +77,13 @@ func (streams project) Run(ctx Ctx, inp, out chan Dataset) (err error) {
     }
 
     return err
+}
+
+// Returns a concatenation of all of the inner runners
+func (rs project) Returns() []Type {
+    rets := []Type{}
+    for _, r := range rs {
+        rets = append(rets, r.Returns()...)
+    }
+    return rets
 }
