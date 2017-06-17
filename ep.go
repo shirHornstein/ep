851facd39a2bf15dc99941ae5a20b1222764f0a6
Roi Avinoam <avinoamr@gmail.com>
2017-06-17 21:52:13 +0300

    Document the registries at the top

--- ep.go
+++ ep.go
@@ -2,6 +2,16 @@
 // pronounced) Epsilon, ep is designed to make it easy to construct complex
 // query engines and data processing pipelines that are distributed across a
 // cluster of nodes.
+//
+// Registries
+//
+// In order to support modular systems design, ep ships with a builtin
+// registries for Runners and Types. These registries support simple Set and Get
+// functions:
+//
+//      func (reg typesReg) Get(name string) Type
+//      func (reg typesReg) Add(name string, t Type) int
+//
 package ep
 
 import (
