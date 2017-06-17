2030550390b6e6de9123476d769ed6f765ab07b1
Roi Avinoam <avinoamr@gmail.com>
2017-06-17 21:18:00 +0300

    Test the example docs generation

--- data_test.go
+++ data_test.go
@@ -4,15 +4,17 @@ package ep
 //     "testing"
 // )
 
+type StringsData []string
+func (d StringsData) Len() int {
+    return len(d)
+}
+
+func (d StringsData) Less(i, j int) bool { return d[i] < d[j] }
+func (d StringsData) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
+func (d StringsData) Slice(s, e int) Data { return d[s:e] }
+func (d StringsData) Append(other Data) Data { return append(d, other.(StringsData)...) }
+func (d StringsData) Strings() []string { return d }
+
 func ExampleData() {
-    type StringsData []string
-    func (d StringsData) Len() int {
-        return len(d)
-    }
-    
-    func (d StringsData) Less(i, j int) bool { return d[i] < d[j] }
-    func (d StringsData) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
-    func (d StringsData) Slice(s, e int) Data { return d[s:e] }
-    func (d StringsData) Append(other Data) Data { return append(d, other.(StringsData)...) }
-    func (d StringsData) Strings() []string { return d }
+
 }
