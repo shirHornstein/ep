b6b1a8d2de738084ab9f99a0eeb5731e69aa7b9d
Roi Avinoam <avinoamr@gmail.com>
2017-06-17 21:19:24 +0300

    Test the example docs generation

--- data_test.go
+++ data_test.go
@@ -1,19 +1,27 @@
 package ep
 
-// import (
-//     "testing"
-// )
+type intType struct {}
+func (*intType) Name() string { return "int4" }
+func (*intType) Data(n uint) ep.Data { return make(Ints, n) }
 
-type StringsData []string
-func (d StringsData) Len() int {
-    return len(d)
+type Ints []int
+func (Ints) Type() ep.Type { return &intType{} }
+func (vs Ints) Len() int { return len(vs) }
+func (vs Ints) Swap(i, j int) { vs[i], vs[j] = vs[j], vs[i] }
+func (vs Ints) Less(i, j int) bool { return vs[i] < vs[j] }
+func (vs Ints) Slice(start, end int) ep.Data { return Ints(vs[start:end]) }
+
+func (vs Ints) Append(data ep.Data) ep.Data {
+    return append(vs, data.(Ints)...)
 }
 
-func (d StringsData) Less(i, j int) bool { return d[i] < d[j] }
-func (d StringsData) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
-func (d StringsData) Slice(s, e int) Data { return d[s:e] }
-func (d StringsData) Append(other Data) Data { return append(d, other.(StringsData)...) }
-func (d StringsData) Strings() []string { return d }
+func (vs Ints) Strings() []string {
+    s := make([]string, len(vs))
+    for i, d := range vs {
+        s[i] = strconv.Itoa(d)
+    }
+    return s
+}
 
 func ExampleData() {
 
