ca3cd45d3305e5cb8ba5954a2e5defea3521480e
Roi Avinoam <avinoamr@gmail.com>
2017-06-17 21:55:06 +0300

    Document the registries at the top

--- reg.go
+++ reg.go
@@ -3,14 +3,14 @@ package ep
 var Types = typesReg{}
 var Runners = runnersReg{}
 
-type typesReg map[string]Type
+type typesReg map[string]Type // see Registries above.
 func (reg typesReg) Get(name string) Type { return reg[name] }
 func (reg typesReg) Add(name string, t Type) int {
     reg[name] = t
     return len(reg)
 }
 
-type runnersReg map[string]Runner
+type runnersReg map[string]Runner // see Registries above.
 func (reg runnersReg) Get(name string) Runner { return reg[name] }
 func (reg runnersReg) Add(name string, r Runner) int {
     reg[name] = r
