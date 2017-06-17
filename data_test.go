5d60c247e4224e2e7db7322647d2d1af1b6ab452
Roi Avinoam <avinoamr@gmail.com>
2017-06-18 02:01:38 +0300

    Finish the basic Data example

--- data_test.go
+++ data_test.go
@@ -1,13 +1,28 @@
 package ep
 
 import (
+    "sort"
     "fmt"
 )
 
 type StrType struct {}
+func (*StrType) Name() string { return "string" }
+func (*StrType) Data(n uint) Data { return make(Strs, n) }
+
+type Strs []string
+func (Strs) Type() Type { return &StrType{} }
+func (vs Strs) Len() int { return len(vs) }
+func (vs Strs) Less(i, j int) bool { return vs[i] < vs[j] }
+func (vs Strs) Swap(i, j int) { vs[i], vs[j] = vs[j], vs[i] }
+func (vs Strs) Slice(s, e int) Data { return vs[s:e] }
+func (vs Strs) Strings() []string { return vs }
+func (vs Strs) Append(o Data) Data { return append(vs, o.(Strs)...) }
 
 
 func ExampleData() {
-    fmt.Println("Hello")
-    // Output: Hello
+    var strs Data = Strs([]string{"hello", "world", "foo", "bar"})
+    sort.Sort(strs)
+    strs = strs.Slice(0, 2)
+    fmt.Println(strs.Strings())
+    // Output: [bar foo]
 }
