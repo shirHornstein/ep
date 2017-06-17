d06f1d0e0041da3274f7776c2fa0f75701cff216
Roi Avinoam <avinoamr@gmail.com>
2017-06-18 02:37:20 +0300

    pipeline test

---
+++ pipeline_test.go
@@ -0,0 +1,23 @@
+package ep
+
+import (
+    "fmt"
+    "context"
+)
+
+func ExamplePipeline() {
+    runner := Pipeline(&Upper{}, &Question{})
+    inp := make(chan Dataset, 1)
+    inp <- NewDataset(Strs([]string{"hello", "world"}))
+    close(inp)
+
+    out := make(chan Dataset)
+    go func() {
+        runner.Run(context.Background(), inp, out)
+        close(out)
+    }()
+
+    for data := range out {
+        fmt.Println(data.At(0)) // Output: [is HELLO? is WORLD?]
+    }
+}
