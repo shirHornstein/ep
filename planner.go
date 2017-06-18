c4c1bd6f59a3b938ce8a4465589c7c6b4018a234
Roi Avinoam <avinoamr@gmail.com>
2017-06-19 01:52:07 +0300

    Docs and cleanup

--- planner.go
+++ planner.go
@@ -7,47 +7,21 @@ import (
 )
 
 // Planner implements Registry and RunnerPlan, and uses the registered Runners
-// for planning based on their configured key argument.
-//
-// Plan creates a Runner for the provided arg. It uses the list of
+// for planning based on their configured key argument. Its Plan() function
+// creates a Runner for the provided arg. It uses the list of
 // registered Runners (see .Runner()) to lookup the relevant Runner. If the
-// resolved Runner implements a similarily typed Plan() function, it's first
-// called with the same arguments to allow the Runner to plan itself.
+// resolved Runner implements RunnerPlan, it's first called with the same
+// arguments to allow the Runner to plan itself.
 //
 // A key can be bound to multiple runners, in which case the Runners will be
-// resolved as a list of middleware - callling each one's Plan method, in
-// turn until one of them doesn't return and error. This means that, upon
-// error, Plan will fallthrough to the next Runner. This allows an
-// opportunistic design where several runners bind to the same node, each
-// planning it differently - if they can.
-// Plan(ctx context.Context, k interface{}) (Runner, error)
-var Planner = newPlanner()
-
-// Registry is a utility object that stores Types and Runners by an arbitrary
-// key argument. Its purpose is to destignate a lookup key used for finding the
-// relevant Runner or Type to be used. You can think of it like a key-value
-// store with the caveat that - if it's not a string - the key is first
-// reflected to the type name of the object, which is then used instead. This
-// means that we can register and use key objects, instead of simple strings.
+// resolved as a list of middlewares - callling each Plan() method in turn until
+// one of them doesn't return and error. This means that, upon error, Plan will
+// fallthrough to the next Runner. This allows an opportunistic design where
+// several runners bind to the same node, each planning it differently - if they
+// can.
 //
-// For example, if we're using SQL to construct Runners, we can bind an AST
-// node:
-//
-//      Planner.Runner(ast.SelectStmt{}, &SelectRunner{})
-//      Planner.Plan(nil, ast.SelectStmt{}) // Returns &SelectRunner{}
-//
-type Registry interface {
-
-    // Type registers and returns a list of Types, marked by a key. Calling this
-    // function without any types will just return all of the registered types
-    Types(k interface{}, types ...Type) []Type
-
-    // Runner registers and returns a list of Runners, marked by a key. Calling
-    // this function without any Runners will just return all of the registered
-    // runners.
-    Runners(k interface{}, runners ...Runner) []Runner
-}
-
+// It cannot be directly used as a Runner
+var Planner = newPlanner() // Registry & RunnerPlan
 
 type planner struct {
     runners map[string][]Runner
@@ -106,6 +80,8 @@ func (p *planner) Runners(k interface{}, runners ...Runner) []Runner {
     return p.runners[s]
 }
 
+// convert an arbitrary key interface to a string - if it's an object, use the
+// object's unique path and name instead.
 func keyToString(k interface{}) string {
     s, ok := k.(string)
     if ok {
