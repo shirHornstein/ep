1fc0dd17c3a5c529efcbc3db79a0410cc7984e77
Roi Avinoam <avinoamr@gmail.com>
2017-06-18 12:24:42 +0300

    Pipeline reversed

--- pipeline_test.go
+++ pipeline_test.go
@@ -21,3 +21,20 @@ func ExamplePipeline() {
         fmt.Println(data.At(0)) // Output: [is HELLO? is WORLD?]
     }
 }
+
+func ExamplePipeline_Reverse() {
+    runner := Pipeline(&Question{}, &Upper{})
+    inp := make(chan Dataset, 1)
+    inp <- NewDataset(Strs([]string{"hello", "world"}))
+    close(inp)
+
+    out := make(chan Dataset)
+    go func() {
+        runner.Run(context.Background(), inp, out)
+        close(out)
+    }()
+
+    for data := range out {
+        fmt.Println(data.At(0)) // Output: [IS HELLO? IS WORLD?]
+    }
+}
