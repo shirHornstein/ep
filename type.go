067d3bbc74d5389d54c7835049204dd2a60f6da9
Roi Avinoam <avinoamr@gmail.com>
2017-06-19 13:35:57 +0300

    Types equality is loosly defined - not sure if we need it generically

--- type.go
+++ type.go
@@ -33,17 +33,6 @@ type anyType struct {}
 func (*anyType) Name() string { return "?" }
 func (*anyType) Data(uint) Data { panic("any has no concrete data") }
 
-// EqualTypes determines if two types are the same, and returns the first if
-// they are, or nil if they aren't.
-func EqualType(t1, t2 Type) (t Type, ok bool) {
-    ok = t1.Name() == t2.Name()
-    if ok {
-        return t1, true
-    } else {
-        return nil, false
-    }
-}
-
 // As returns a new Type that's assigned a name, Useful for cases where the name
 // of the data represented by the type matters for later referencing. To fetch
 // the name, cast the type to `interface { As() string }`
