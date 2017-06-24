0a13459e217b4e6ccbe77d64c25169af2b66e6b2
Roi Avinoam <avinoamr@gmail.com>
2017-06-24 14:46:42 +0300

    Scaffolding for exchange testing

---
+++ exchange_test.go
@@ -0,0 +1,15 @@
+package ep
+
+import (
+    "fmt"
+)
+
+func ExampleExchange() {
+    runner := PassThrough()
+    data1 := NewDataset(Strs{"hello", "world"})
+    data2 := NewDataset(Strs{"foo", "bar"})
+    data, err := testRun(runner, data1, data2)
+    fmt.Println(data, err)
+
+    // Output: [[hello world foo bar]] <nil>
+}
