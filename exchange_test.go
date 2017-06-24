bfee77c62a02b9a41cffa5657b34d09e0c03e040
Roi Avinoam <avinoamr@gmail.com>
2017-06-24 16:43:48 +0300

    Test scatter with a single node

--- exchange_test.go
+++ exchange_test.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "net"
     "testing"
+    "github.com/stretchr/testify/require"
 )
 
 func ExampleScatter_single() {
@@ -22,12 +23,36 @@ func ExampleScatter_single() {
     // Output: [[hello world foo bar]] <nil>
 }
 
+// Tests the scattering when there's just one node - the whole thing should
+// be short-circuited to act as a pass-through
+func TestScatterSingleNode(t *testing.T) {
+    ln, err := net.Listen("tcp", ":5551")
+    require.NoError(t, err)
+
+    dist := NewDistributer(ln)
+    go dist.Start()
+    defer dist.Close()
+
+    data1 := NewDataset(Strs{"hello", "world"})
+    data2 := NewDataset(Strs{"foo", "bar"})
+    runner := Pipeline(PassThrough(), Scatter())
+    runner, err = dist.Distribute(runner, ":5551", ":5551")
+    require.NoError(t, err)
+
+    data, err := testRun(runner, data1, data2)
+    require.NoError(t, err)
+    require.Equal(t, 1, data.Width())
+    require.Equal(t, 4, data.Len())
+}
+
 
 func TestScatterMulti(t *testing.T) {
     ln, _ := net.Listen("tcp", ":5551")
     dist := NewDistributer(ln)
     defer dist.Close()
     go dist.Start()
+
+
 //
 //     data1 := NewDataset(Strs{"hello", "world"})
 //     data2 := NewDataset(Strs{"foo", "bar"})
