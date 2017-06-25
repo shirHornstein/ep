6516cc3f28084da97f03a01470646ee2d0c5aa6f
Roi Avinoam <avinoamr@gmail.com>
2017-06-25 21:26:18 +0300

    Scatter Example with two nodes and half the output data

--- exchange_test.go
+++ exchange_test.go
@@ -11,19 +11,24 @@ import (
 type errDialer struct { net.Listener; Err error }
 func (e *errDialer) Dial(addr string) (net.Conn, error) { return nil, e.Err }
 
-func ExampleScatter_single() {
-    ln, _ := net.Listen("tcp", ":5551")
-    dist := NewDistributer(":5551", ln)
-    go dist.Start()
-    defer dist.Close()
+func ExampleScatter() {
+    ln1, _ := net.Listen("tcp", ":5551")
+    dist1 := NewDistributer(":5551", ln1)
+    go dist1.Start()
+    defer dist1.Close()
+
+    ln2, _ := net.Listen("tcp", ":5552")
+    dist2 := NewDistributer(":5552", ln2)
+    go dist2.Start()
+    defer dist2.Close()
 
     data1 := NewDataset(Strs{"hello", "world"})
     data2 := NewDataset(Strs{"foo", "bar"})
-    runner, _ := dist.Distribute(Scatter(), ":5551", ":5551")
+    runner, _ := dist1.Distribute(Scatter(), ":5551", ":5551", ":5552")
     data, err := testRun(runner, data1, data2)
-    fmt.Println(data, err)
+    fmt.Println(data, err) // no gather - only one batch should return
 
-    // Output: [[hello world foo bar]] <nil>
+    // Output: [[foo bar]] <nil>
 }
 
 // Test that errors are transmitted across the network (an error in one node
@@ -83,32 +88,4 @@ func TestScatterSingleNode(t *testing.T) {
     require.NoError(t, err)
     require.Equal(t, 1, data.Width())
     require.Equal(t, 4, data.Len())
-}
-
-
-func TestScatterMulti(t *testing.T) {
-    ln1, err := net.Listen("tcp", ":5551")
-    require.NoError(t, err)
-
-    dist1 := NewDistributer(":5551", ln1)
-    defer dist1.Close()
-    go dist1.Start()
-
-    ln2, err := net.Listen("tcp", ":5552")
-    require.NoError(t, err)
-
-    dist2 := NewDistributer(":5552", ln2)
-    defer dist2.Close()
-    go dist2.Start()
-
-    data1 := NewDataset(Strs{"hello", "world"})
-    data2 := NewDataset(Strs{"foo", "bar"})
-    runner, err := dist1.Distribute(Scatter(), ":5551", ":5551", ":5552")
-    require.NoError(t, err)
-
-    data, err := testRun(runner, data1, data2)
-    require.NoError(t, err)
-
-    // there's no gather, so only one of the batches should arrive here.
-    require.Equal(t, "[[foo bar]]", fmt.Sprintf("%v", data))
 }
