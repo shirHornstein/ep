06a8a25ab3ba3a4e902bb8b398ab17a618207ca3
Roi Avinoam <avinoamr@gmail.com>
2017-07-20 15:28:24 +0300

    Comment about the length of Datasets

--- project.go
+++ project.go
@@ -10,7 +10,8 @@ var errMismatch = fmt.Errorf("ep.Project: mismatched number of rows")
 
 // Project returns a horizontal composite projection runner that dispatches
 // its input to all of the internal runners, and joins the result into a single
-// dataset to return.
+// dataset to return. It is required that all runners produce Datasets of the
+// same length.
 func Project(runners ...Runner) Runner {
 	if len(runners) == 0 {
 		panic("at least 1 runner is required for projecting")
