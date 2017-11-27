45f82d206ec845fc5dd622382308e58338c6f25a
aviaoh <avia@panoply.io>
2017-11-27 09:24:30 +0200

    add sortableDataset to handle recurring columns

--- dataset_test.go
+++ dataset_test.go
@@ -1,8 +1,30 @@
 package ep
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"sort"
+	"testing"
+)
 
 func TestDataInterface(t *testing.T) {
-	dataset := NewDataset(Null.Data(10), str.Data(10))
+	dataset := NewDataset(Null.Data(10), str.Data(10)).GetSortable()
 	VerifyDataInvariant(t, dataset)
+}
+
+func TestDatasetSort(t *testing.T) {
+	var d1 Data = strs([]string{"hello", "world", "foo", "bar"})
+	var d2 Data = strs([]string{"1", "2", "4", "3"})
+
+	dataset := newSortableDataset(dataset{d1, d2, d1, d2})
+	sort.Sort(dataset)
+
+	require.Equal(t, 4, dataset.Len())
+	require.Equal(t, 4, dataset.Width())
+
+	// sorting always done according last column
+	require.Equal(t, "[1 2 3 4]", fmt.Sprintf("%+v", dataset.At(dataset.Width()-1)))
+	// verify other columns were updated as well
+	require.Equal(t, "[hello world bar foo]", fmt.Sprintf("%+v", dataset.At(0)))
+	require.Equal(t, "[1 2 3 4]", fmt.Sprintf("%+v", dataset.At(1)))
 }
