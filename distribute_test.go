cfa081c992f47d0fa4fc7fe7e7c17c68ad1fd3f0
aviaoh <avia@panoply.io>
2018-01-11 16:01:28 +0200

    tests

--- distribute_test.go
+++ distribute_test.go
@@ -14,14 +14,14 @@ func TestDistribute_success(t *testing.T) {
 	// avoid "bind: address already in use" error in future tests
 	defer time.Sleep(1 * time.Millisecond)
 
-	port1 := ":5551"
+	port1 := ":5561"
 	dist1 := mockPeer(t, port1)
 	defer dist1.Close()
 
-	port2 := ":5552"
+	port2 := ":5562"
 	defer mockPeer(t, port2).Close()
 
-	port3 := ":5553"
+	port3 := ":5563"
 	defer mockPeer(t, port3).Close()
 
 	runner := dist1.Distribute(Pipeline(Scatter(), Gather()), port1, port2, port3)
@@ -39,16 +39,16 @@ func TestDistribute_connectionError(t *testing.T) {
 	// avoid "bind: address already in use" error in future tests
 	defer time.Sleep(1 * time.Millisecond)
 
-	port1 := ":5551"
+	port1 := ":5561"
 	dist1 := mockPeer(t, port1)
 	defer dist1.Close()
 
-	runner := dist1.Distribute(&upper{}, port1, ":5552")
+	runner := dist1.Distribute(&upper{}, port1, ":5000")
 
 	data, err := TestRunner(runner, NewDataset())
 
 	require.Error(t, err)
-	require.Equal(t, "dial tcp :5552: getsockopt: connection refused", err.Error())
+	require.Equal(t, "dial tcp :5000: getsockopt: connection refused", err.Error())
 	require.Nil(t, data)
 }
 
@@ -57,11 +57,11 @@ func TestDistribute_errorFromPeer(t *testing.T) {
 	// avoid "bind: address already in use" error in future tests
 	defer time.Sleep(1 * time.Millisecond)
 
-	port1 := ":5551"
+	port1 := ":5561"
 	dist1 := mockPeer(t, port1)
 	defer dist1.Close()
 
-	port2 := ":5552"
+	port2 := ":5562"
 	defer mockPeer(t, port2).Close()
 
 	mightErrored := &dataRunner{NewDataset(), port2}
@@ -70,32 +70,6 @@ func TestDistribute_errorFromPeer(t *testing.T) {
 	data1 := NewDataset(strs{"hello", "world"})
 	data2 := NewDataset(strs{"foo", "bar"})
 	data, err := TestRunner(runner, data1, data2)
-
-	require.Error(t, err)
-	require.Equal(t, "error", err.Error())
-	require.Equal(t, 0, data.Width())
-}
-
-// Test that errors are transmitted across the network
-func _TestDistribute_errorFromPeerStopAllPeers(t *testing.T) {
-	port1 := ":5551"
-	dist1 := mockPeer(t, port1)
-	defer dist1.Close()
-
-	port2 := ":5552"
-	defer mockPeer(t, port2).Close()
-
-	port3 := ":5553"
-	defer mockPeer(t, port3).Close()
-
-	mightErrored := &dataRunner{NewDataset(), port2}
-	runner := dist1.Distribute(Pipeline(Scatter(), &nodeAddr{}, mightErrored, Gather()), port1, port2, port3)
-
-	data1 := NewDataset(strs{"hello", "world"})
-	data2 := NewDataset(strs{"foo", "bar"})
-	data, err := TestRunner(runner, data1, data2, data2, data2, data2, data2, data2, data2, data2, data2,
-		data2, data2, data2, data2, data2, data2, data2, data2, data2, data2, data2, data2, data2, data2,
-		data2, data2, data2, data2, data2, data2, data2, data2, data2, data2, data2)
 
 	require.Error(t, err)
 	require.Equal(t, "error", err.Error())
