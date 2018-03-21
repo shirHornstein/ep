69c7cd76a2975f56cd646b4aa8b89f79563e3782
shirHornstein <shir@panoply.io>
2018-03-21 14:46:18 +0200

    cr fixes

--- eptest/data.go
+++ eptest/data.go
@@ -59,43 +59,34 @@ func VerifyDataInterfaceInvariant(t *testing.T, data ep.Data) {
 	require.Equal(t, dataString, fmt.Sprintf("%+v", data))
 }
 
-// VerifyNullsHandling makes sure all functions handel nulls
-func VerifyNullsHandling(t *testing.T, typ ep.Type) {
+// VerifyDataNullsHandling makes sure all functions handle nulls
+func VerifyDataNullsHandling(t *testing.T, typ ep.Type) {
 	data := typ.Data(dataLength)
 	nullIdx := 1
 
-	t.Run("TestData_IsNull_NoNulls", func(t *testing.T) {
+	t.Run("TestData_IsNull_noNulls", func(t *testing.T) {
 		isNull := data.IsNull(nullIdx)
 		require.False(t, isNull)
 	})
 
 	data.MarkNull(nullIdx)
 
-	t.Run("TestData_IsNull_WithNulls", func(t *testing.T) {
+	t.Run("TestData_IsNull_withNulls", func(t *testing.T) {
 		isNull := data.IsNull(nullIdx)
 		require.True(t, isNull)
 	})
 
-	t.Run("TestData_Equal_WithNulls", func(t *testing.T) {
-		isEqual := data.Equal(data)
-		require.True(t, isEqual)
+	t.Run("TestData_Type_withNulls", func(t *testing.T) {
+		dataType := data.Type()
+		require.Equal(t, typ, dataType)
 	})
 
-	t.Run("TestData_IsNull_WithNulls", func(t *testing.T) {
+	t.Run("TestData_Len_withNulls", func(t *testing.T) {
 		size := data.Len()
 		require.Equal(t, dataLength, size)
 	})
 
-	t.Run("TestData_Swap_WithNulls", func(t *testing.T) {
-		data.Swap(0, nullIdx)
-		require.True(t, data.IsNull(0))
-		require.False(t, data.IsNull(nullIdx))
-		data.Swap(0, nullIdx)
-		require.True(t, data.IsNull(nullIdx))
-		require.False(t, data.IsNull(0))
-	})
-
-	t.Run("TestData_IsLess_WithNulls", func(t *testing.T) {
+	t.Run("TestData_Less_withNulls", func(t *testing.T) {
 		isLess := data.Less(0, nullIdx)
 		require.True(t, isLess)
 		isLess = data.Less(nullIdx, 0)
@@ -104,32 +95,41 @@ func VerifyNullsHandling(t *testing.T, typ ep.Type) {
 		require.False(t, isLess)
 	})
 
-	t.Run("TestData_Type_WithNulls", func(t *testing.T) {
-		dataType := data.Type()
-		require.Equal(t, typ, dataType)
+	t.Run("TestData_Swap_withNulls", func(t *testing.T) {
+		data.Swap(0, nullIdx)
+		require.True(t, data.IsNull(0))
+		require.False(t, data.IsNull(nullIdx))
+		data.Swap(0, nullIdx)
+		require.True(t, data.IsNull(nullIdx))
+		require.False(t, data.IsNull(0))
 	})
 
-	t.Run("TestData_Slice_WithNulls", func(t *testing.T) {
+	t.Run("TestData_Slice_withNulls", func(t *testing.T) {
 		slicedData := data.Slice(0, data.Len()/2)
 		require.True(t, slicedData.IsNull(nullIdx))
 	})
 
-	t.Run("TestData_Append_WithNulls", func(t *testing.T) {
-		appendData := data.Append(data)
-		require.True(t, appendData.IsNull(nullIdx))
-		require.True(t, appendData.IsNull(nullIdx+dataLength))
+	t.Run("TestData_Append_withNulls", func(t *testing.T) {
+		appendedData := data.Append(data)
+		require.True(t, appendedData.IsNull(nullIdx))
+		require.True(t, appendedData.IsNull(nullIdx+dataLength))
 	})
 
-	t.Run("TestData_Duplicate_WithNulls", func(t *testing.T) {
-		duplicateData := data.Duplicate(3)
-		require.True(t, duplicateData.IsNull(nullIdx))
-		require.True(t, duplicateData.IsNull(nullIdx+dataLength))
-		require.True(t, duplicateData.IsNull(nullIdx+2*dataLength))
-		require.False(t, duplicateData.IsNull(2*dataLength))
+	t.Run("TestData_Duplicate_withNulls", func(t *testing.T) {
+		duplicatedData := data.Duplicate(3)
+		require.True(t, duplicatedData.IsNull(nullIdx))
+		require.True(t, duplicatedData.IsNull(nullIdx+dataLength))
+		require.True(t, duplicatedData.IsNull(nullIdx+2*dataLength))
+		require.False(t, duplicatedData.IsNull(2*dataLength))
 	})
 
-	t.Run("TestData_Strings_WithNulls", func(t *testing.T) {
-		s := data.Strings()
-		require.True(t, len(s[nullIdx]) == 0)
+	t.Run("TestData_Equal_withNulls", func(t *testing.T) {
+		isEqual := data.Equal(data)
+		require.True(t, isEqual)
+	})
+
+	t.Run("TestData_Strings_withNulls", func(t *testing.T) {
+		strings := data.Strings()
+		require.True(t, len(strings[nullIdx]) == 0)
 	})
 }
