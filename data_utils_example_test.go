b23e24aedc8a352f583c416705ccfd155bb48e91
aviaoh <avia@panoply.io>
2018-07-25 15:38:23 +0300

    fix dataset.Append to not affect "other" argument

--- data_utils_example_test.go
+++ data_utils_example_test.go
@@ -5,14 +5,14 @@ import (
 	"github.com/panoplyio/ep"
 )
 
-func ExampleClone() {
-	var d1 ep.Data = strs([]string{"hello", "world"})
-	d2 := ep.Clone(d1).(strs)
-
+func ExampleDataset_Duplicate() {
+	var dataset1 = ep.NewDataset(strs([]string{"hello", "world"}))
+	dataset2 := dataset1.Duplicate(1).(ep.Dataset)
+	d2 := dataset2.At(0).(strs)
 	d2[0] = "foo"
 	d2[1] = "bar"
-	fmt.Println(d2) // clone modified
-	fmt.Println(d1) // original left intact
+	fmt.Println(dataset2.At(0)) // clone modified
+	fmt.Println(dataset1.At(0)) // original left intact
 
 	// Output:
 	// [foo bar]
