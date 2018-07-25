d2ea21e63f3116367a77fb85fa1f922d0a9e11d7
aviaoh <avia@panoply.io>
2018-07-25 16:08:12 +0300

    move example to dataset_test

--- data_utils_example_test.go
+++ data_utils_example_test.go
@@ -5,20 +5,6 @@ import (
 	"github.com/panoplyio/ep"
 )
 
-func ExampleDataset_Duplicate() {
-	var dataset1 = ep.NewDataset(strs([]string{"hello", "world"}))
-	dataset2 := dataset1.Duplicate(1).(ep.Dataset)
-	d2 := dataset2.At(0).(strs)
-	d2[0] = "foo"
-	d2[1] = "bar"
-	fmt.Println(dataset2.At(0)) // clone modified
-	fmt.Println(dataset1.At(0)) // original left intact
-
-	// Output:
-	// [foo bar]
-	// [hello world]
-}
-
 func ExampleCut() {
 	var d ep.Data = strs([]string{"hello", "world", "foo", "bar"})
 	data := ep.Cut(d, 1, 3)
