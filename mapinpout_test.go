65761cbedc2409820a8b7b099ec756ad650b89bc
aviaoh <avia@panoply.io>
2018-08-28 11:26:07 +0300

    Revert "Remove MapInpToOut"

---
+++ mapinpout_test.go
@@ -0,0 +1,19 @@
+package ep_test
+
+import (
+	"fmt"
+	"github.com/panoplyio/ep"
+	"github.com/panoplyio/ep/eptest"
+)
+
+func ExampleMapInpToOut() {
+	// breakChars is a runner that breaks the input strings into characters (one
+	// character per row).
+	r := ep.MapInpToOut(&breakChars{})
+	data := ep.NewDataset(strs([]string{"XY", "ABC"}))
+	data, err := eptest.Run(r, data)
+	fmt.Println(data.Strings(), err)
+
+	// Output:
+	// [(XY,X) (XY,Y) (ABC,A) (ABC,B) (ABC,C)] <nil>
+}
