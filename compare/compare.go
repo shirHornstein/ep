74ddd10b31167c30800db8b6aa00dad4eef2817a
bora <boris@panoply.io>
2019-01-06 14:19:23 +0200

    Using iota in the 'Comparison' enum

--- compare/compare.go
+++ compare/compare.go
@@ -5,13 +5,13 @@ type Comparison byte
 
 const (
 	// Equal represents equal to other data object, e.g. 1 == 1
-	Equal Comparison = 'a'
+	Equal Comparison = iota + 1
 	// BothNulls represents a case in which the current & the other data objects are null, e.g. null == null
-	BothNulls Comparison = 'b'
+	BothNulls
 	// Null represents a case in which only 1 of the data objects is null, e.g. 1 == null
-	Null Comparison = 'c'
+	Null
 	// Greater represents greater than other data object, e.g. 2 > 1
-	Greater Comparison = 'd'
+	Greater
 	// Less represents less than other data object, e.g. 1 < 2
-	Less Comparison = 'e'
+	Less
 )
