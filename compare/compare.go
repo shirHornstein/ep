e41df77912fdc50c452ea7d51579c72d36d4d3db
bora <boris@panoply.io>
2019-01-09 12:09:52 +0200

    Modify 'Result' description

--- compare/compare.go
+++ compare/compare.go
@@ -4,18 +4,31 @@ package compare
 type Result byte
 
 const (
-	// Equal represents equality to other data object, e.g. 1 == 1
+	// NOTE: Result const (Equal / BothNulls / Null...) is the outcome of
+	// a comparison between 2 Rows.
+	//
+	// Rows - is the Value inside the Data.
+	// Data - is all the column's data.
+	// Data Object - is the column.
+	//
+	// E.g. ((1,'Bob'), (2,'Danny'), (3,'John')) AS t1(id,name)
+	// Data Object: name
+	// Data: 'Bob', 'Danny', 'John'
+	// Row: 'Danny'
+
+	// Equal represents equality of the current value to other value
+	// e.g. 1 == 1
 	Equal Result = iota + 1
-	// BothNulls represents a case in which the current & the other data objects
-	// are null, e.g. null == null
-	// Note: The null refers to both Rows (the Value inside the Data)
+	// BothNulls represents a case in which the value and the other value
+	// are both nulls, e.g. null == null
 	BothNulls
-	// Null represents a case in which only 1 of the data objects is null,
+	// Null represents a case in which only 1 of the values is null
 	// e.g. 1 == null
-	// Note: The null refers to the Row (the Value inside the Data)
 	Null
-	// Greater represents greater than other data object, e.g. 2 > 1
+	// Greater represents greater than other value
+	// e.g. 2 > 1
 	Greater
-	// Less represents smaller value than value, e.g. 1 < 2
+	// Less represents smaller value than other value
+	// e.g. 1 < 2
 	Less
 )
