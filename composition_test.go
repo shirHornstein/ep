29c5b1e49e954142ebbd79c407c23b6e6ef43423
yury <yury@panoply.io>
2019-01-21 11:59:24 +0200

    Improve composition unit tests
    - Rename subtests;
    
    - Change ComposeProject test to only depend on ComposeProject
    implementation, and not on Compose+ComposeProject together;
    - Cover a case where one of the Composables is itself a ComposeProject.

--- composition_test.go
+++ composition_test.go
@@ -7,12 +7,12 @@ import (
 	"testing"
 )
 
-func TestComposition(t *testing.T) {
+func TestCompose(t *testing.T) {
 	c1 := integers{1, 2, 3, 4, 5}
 	c2 := integers{10, 20, 30, 40, 50}
 	input := ep.NewDataset(c1, c2)
 
-	t.Run("OnePiece", func(t *testing.T) {
+	t.Run("single Composable", func(t *testing.T) {
 		runner := ep.Compose(
 			[]ep.Type{integer},
 			&addInts{},
@@ -26,7 +26,7 @@ func TestComposition(t *testing.T) {
 		require.Equal(t, expected, res.At(0).Strings())
 	})
 
-	t.Run("TwoPieces", func(t *testing.T) {
+	t.Run("multiple Composables", func(t *testing.T) {
 		runner := ep.Compose(
 			[]ep.Type{integer},
 			&addInts{}, &negateInt{},
@@ -45,25 +45,25 @@ func TestComposeProject(t *testing.T) {
 	col := integers{1, 2, 3, 4, 5}
 	input := ep.NewDataset(col)
 
-	comp1 := ep.Compose(
-		[]ep.Type{integer},
-		&negateInt{}, &mulIntBy2{},
-	).(ep.Composable)
-	comp2 := ep.Compose(
-		[]ep.Type{integer},
-		&mulIntBy2{}, &mulIntBy2{}, &negateInt{},
-	).(ep.Composable)
-	project := ep.ComposeProject(comp1, comp2)
+	proj1 := &negateInt{}
+	proj2 := ep.ComposeProject(&mulIntBy2{}, &negateInt{})
+	proj3 := &mulIntBy2{}
 
-	composition := ep.Compose(
-		[]ep.Type{integer, integer},
-		project, &addInts{},
-	)
-	expected := []string{"-6", "-12", "-18", "-24", "-30"}
+	project := ep.ComposeProject(proj1, proj2, proj3)
+	batchFunction := project.BatchFunction()
 
-	res, err := eptest.Run(composition, input)
+	expected1 := []string{"-1", "-2", "-3", "-4", "-5"}
+	expected2 := []string{"2", "4", "6", "8", "10"}
+	expected3 := []string{"-1", "-2", "-3", "-4", "-5"}
+	expected4 := []string{"2", "4", "6", "8", "10"}
+
+	res, err := batchFunction(input)
 	require.NoError(t, err)
-	require.Equal(t, 1, res.Width())
+	require.Equal(t, 4, res.Width())
 	require.Equal(t, input.Len(), res.Len())
-	require.Equal(t, expected, res.At(0).Strings())
+
+	require.Equal(t, expected1, res.At(0).Strings())
+	require.Equal(t, expected2, res.At(1).Strings())
+	require.Equal(t, expected3, res.At(2).Strings())
+	require.Equal(t, expected4, res.At(3).Strings())
 }
