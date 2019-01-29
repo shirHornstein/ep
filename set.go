6846a113e518a633e45b72924e060ee0b073f080
shirHornstein <shir@panoply.io>
2019-01-29 12:21:48 +0200

    fix comment

--- set.go
+++ set.go
@@ -3,7 +3,7 @@ package ep
 // StringsSet holds set of unique strings
 type StringsSet map[string]struct{}
 
-// Contains gets two maps and checks if other map includes in the scopes map
+// Contains checks if other StringsSet included in the current StringsSet
 func (r StringsSet) Contains(other StringsSet) bool {
 	for s := range other {
 		if _, ok := r[s]; !ok {
