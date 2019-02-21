a2028915d6c1d60e3336c4f09f6eb746274dc450
bora <boris@panoply.io>
2019-02-21 14:06:54 +0200

    Modify an existing unit-test, such that scopes is being tested

--- compose_test.go
+++ compose_test.go
@@ -11,11 +11,14 @@ func TestCompose(t *testing.T) {
 	c1 := integers{1, 2, 3, 4, 5}
 	c2 := integers{10, 20, 30, 40, 50}
 	input := ep.NewDataset(c1, c2)
+	expectedScopes := map[string]struct{}{
+		"a": {},
+	}
 
 	t.Run("single Composable", func(t *testing.T) {
 		runner := ep.Compose(
 			[]ep.Type{integer},
-			nil,
+			ep.StringsSet{"a": struct{}{}},
 			&addInts{},
 		)
 		expected := []string{"11", "22", "33", "44", "55"}
@@ -25,12 +28,13 @@ func TestCompose(t *testing.T) {
 		require.Equal(t, 1, res.Width())
 		require.Equal(t, input.Len(), res.Len())
 		require.Equal(t, expected, res.At(0).Strings())
+		require.Equal(t, expectedScopes["a"], runner.(ep.ScopesRunner).Scopes()["a"])
 	})
 
 	t.Run("multiple Composables", func(t *testing.T) {
 		runner := ep.Compose(
 			[]ep.Type{integer},
-			nil,
+			ep.StringsSet{"a": struct{}{}},
 			&addInts{}, &negateInt{},
 		)
 		expected := []string{"-11", "-22", "-33", "-44", "-55"}
@@ -40,6 +44,7 @@ func TestCompose(t *testing.T) {
 		require.Equal(t, 1, res.Width())
 		require.Equal(t, input.Len(), res.Len())
 		require.Equal(t, expected, res.At(0).Strings())
+		require.NotEqual(t, expectedScopes["a"], runner.(ep.ScopesRunner).Scopes()["b"])
 	})
 }
 
